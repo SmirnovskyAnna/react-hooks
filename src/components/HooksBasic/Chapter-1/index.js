5579c024e8564248fb22475ef3916b908d9fc7e8
SmirnovskyAnna <smirnova@appbooster.ru>
2022-05-12 13:14:43 +0300

    added a counter within useState

--- src/components/HooksBasic/Chapter-1/index.js
+++ src/components/HooksBasic/Chapter-1/index.js
@@ -1,5 +1,24 @@
+//The basics of useState. Using a pure function. Standard clicker counter.
 import * as React from "react";
 
 export default function Chapter1() {
-  return <div>Basic hooks</div>;
+  const [clicks, setClicks] = React.useState(0);
+  const [showClicks, setShowClicks] = React.useState(false);
+
+  const onClick = () => {
+    setTimeout(() => {
+      setClicks((prevValue) => prevValue + 1);
+    }, 2000);
+  };
+
+  const toggleShowClicks = () => setShowClicks((prev) => !prev);
+
+  const clicksText = showClicks ? `(${clicks})` : "";
+
+  return (
+    <>
+      <button onClick={onClick}>Click me! {clicksText}</button>
+      <button onClick={toggleShowClicks}>Toggle show clicks</button>
+    </>
+  );
 }
