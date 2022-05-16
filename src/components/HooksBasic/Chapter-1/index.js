b2565a4283b5de9be2ecacd8cb6672cb68af976b
SmirnovskyAnna <smirnova@appbooster.ru>
2022-05-16 12:47:42 +0300

    created input's useState

--- src/components/HooksBasic/Chapter-1/index.js
+++ src/components/HooksBasic/Chapter-1/index.js
@@ -4,12 +4,17 @@ import * as React from "react";
 export default function Chapter1() {
   const [clicks, setClicks] = React.useState(0);
   const [showClicks, setShowClicks] = React.useState(false);
+  const [inputText, setInputText] = React.useState("");
 
   const onClick = () => {
     setTimeout(() => {
       setClicks((prevValue) => prevValue + 1);
     }, 2000);
   };
+
+  const onChange = (e) => {
+    setInputText(e.target.value)
+  }
 
   const toggleShowClicks = () => setShowClicks((prev) => !prev);
 
@@ -19,6 +24,7 @@ export default function Chapter1() {
     <>
       <button onClick={onClick}>Click me! {clicksText}</button>
       <button onClick={toggleShowClicks}>Toggle show clicks</button>
+      <input value={inputText} type="text" onChange={onChange}/>
     </>
   );
 }
